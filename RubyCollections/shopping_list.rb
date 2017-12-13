c26a1506bd2b73397d9edd2d26ba5a932eb5af79
Wisnu Wardhana <abubakar.shiddiq@gmail.com>
2017-12-13 17:39:41 +0700

    add shopping list

--- RubyCollections/shopping_list.rb
+++ RubyCollections/shopping_list.rb
@@ -10,11 +10,35 @@ def add_list_item
   print "What is the item called? "
   item_name = gets.chomp
 
-  hash = { "name" => item_name }
+  print "How much? "
+  quantity = gets.chomp.to_i
+
+  hash = { "name" => item_name, "quantity" => quantity }
   return hash
 end
 
-list = create_list()
-puts list.inspect
+def print_separator(characters="-")
+  puts characters * 80
+end
 
-puts add_list_item().inspect
+def print_list(list)
+  puts "List: #{list['name']}"
+  print_separator()
+
+  list["items"].each do |item|
+    puts "\tItem: " + item['name'] + "\t\t\t" + 
+         "Quantity: " + item['quantity'].to_s
+  end
+  print_separator()
+end
+
+list = create_list()
+
+puts "\nGreat! Add some items to your list."
+list["items"].push(add_list_item())
+list["items"].push(add_list_item())
+list["items"].push(add_list_item())
+
+puts "\nHere's your list:\n"
+print_list(list)
+puts "\n"
