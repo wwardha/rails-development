5269b4f38ec5a767da220748ceb236f5cea5bb63
wwardha <abubakar,shiddiq@gmail.com>
2018-01-16 15:54:58 +0700

    Modify contact list

--- RubyLoops/contact_list.rb
+++ RubyLoops/contact_list.rb
@@ -1,39 +1,39 @@
-def ask(question, kind="string")
-    print question + " "
-    answer = gets.chomp
-    answer = answer.to_i if kind == "number"
-    return answer
+def ask(question, kind='string')
+  print question + " "
+  answer = gets.chomp
+  answer.to_i if kind == 'number'
+  answer
 end
 
 def add_contact
-    contact = {"name" => "", "phone_numbers" => []}
-    contact["name"] = ask("What is the person's name?")
-    answer = ""
-    while answer != "n"
-        answer = ask("Do you want to add a phone number? (y/n)")
-        if answer == "y"
-            phone = ask("Enter a phone number:")
-            contact["phone_numbers"].push(phone)
-        end
+  contact = {'name' => '', 'phone_numbers' => []}
+  contact['name'] = ask('What is the person\'s name?')
+  answer = ''
+  while answer != 'n'
+    answer = ask('Do you want to add a phone number? (y/n)')
+    if answer == 'y'
+      phone = ask('Enter a phone number:')
+      contact['phone_numbers'].push(phone)
     end
-    return contact
+  end
+  contact
 end
 
 contact_list = []
-answer = ""
-while answer != "n"
-    contact_list.push(add_contact())
-    answer = ask("Add another contact? (y/n)")
+answer = ''
+while answer != 'n'
+  contact_list.push(add_contact)
+  answer = ask('Add another contact? (y/n)')
 end
 
 puts "----"
 
 contact_list.each do |contact|
-    puts "Name: #{contact["name"]}"
-    if contact["phone_numbers"].size > 0
-        contact["phone_numbers"].each do |phone_number|
-            puts "Phone Number: #{phone_number}"
-        end
+  puts "Name: #{contact['name']}"
+  if contact['phone_numbers'].size > 0
+    contact['phone_numbers'].each do |phone_number|
+      puts "Phone Number: #{phone_number}"
     end
-    puts "-----\n"
+  end
+  puts "-----\n"
 end
